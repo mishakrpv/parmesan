d9a40e89075c24748217ca9adb267336fadde53a
mishakrpv <mishavkrpv@gmail.com>
2025-03-02 17:27:21 +0300

    feat(*): init solution projects

---
+++ src/Parmesan.LastSeenService.API/Model/LastSeen.cs
@@ -0,0 +1,12 @@
+﻿namespace Parmesan.LastSeenService.API.Model;
+
+public class LastSeen(string userId)
+{
+    public string UserId { get; private set; } = userId;
+    public DateTimeOffset Timestamp { get; private set; } = DateTimeOffset.UtcNow;
+
+    public void UpdateTimestamp()
+    {
+        Timestamp = DateTimeOffset.UtcNow;
+    }
+}
